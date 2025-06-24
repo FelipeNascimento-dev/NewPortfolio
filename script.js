f30859274caf4bb1d64f5bc0bcd98ed3dbbd94d9
Felipe Nascimento <felipe.nasci.dev@gmail.com>
2025-06-23 23:32:19 -0300

    feat: novo portfolio com botao dark mode

---
+++ script.js
@@ -0,0 +1,77 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+// Dark / light mode
+
+const btn = document.getElementById("modeToggle");
+const btn2 = document.getElementById("modeToggle2");
+const themeIcons = document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+  setDarkMode();
+}
+
+btn.addEventListener("click", function () {
+  setTheme();
+});
+
+btn2.addEventListener("click", function () {
+  setTheme();
+});
+
+function setTheme() {
+  let currentTheme = document.body.getAttribute("theme");
+
+  if (currentTheme === "dark") {
+    setLightMode();
+  } else {
+    setDarkMode();
+  }
+}
+
+function setDarkMode() {
+  document.body.setAttribute("theme", "dark");
+  localStorage.setItem("theme", "dark");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-dark");
+  });
+}
+
+function setLightMode() {
+  document.body.removeAttribute("theme");
+  localStorage.setItem("theme", "light");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-light");
+  });
+}
+
+function baixarCV() {
+  const link = document.createElement("a");
+  link.href = "./assets/CurriculoJunho.pdf";
+  link.download = "Curriculo-FelipeNascimento.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function enviarWhats(event) {
+  event.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const mensagem = document.getElementById("mensagem").value;
+  const telefone = "5511951018967";
+
+  const texto = `Olá! Me chamo ${nome}, e ${mensagem}`;
+  const msgFormatada = encodeURIComponent(texto);
+
+  const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+  window.open(url, "_blank");
+}
